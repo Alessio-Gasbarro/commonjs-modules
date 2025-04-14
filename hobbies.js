e152898027441b781f9683d743f2b1779a44dc1a
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-04-14 16:29:43 +0200

    [UPD] Added let to hobbies.js and function to people.js

--- hobbies.js
+++ hobbies.js
@@ -1,4 +1,8 @@
 //Funzione con 3 parametri "hobbyOne" "hobbyTwo" "hobbyThree", + return
+let hobbyOne = 'Gaming';
+let hobbyTwo = 'Cooking';
+let hobbyThree = 'Sleeping';
+
 const hobbyList = (hobbyOne, hobbyTwo, hobbyThree) => {
     return hobbyOne + ' ' + hobbyTwo + ' ' + hobbyThree;
 }
--- people.js
+++ people.js
@@ -1,3 +1,17 @@
 //Importare funzione da names.js & hobbies.js
 const fullname = require('./name.js');
 const hobbyList = require('./hobbies.js');
+
+//Funzione senza parametri usando quello che è stato importato
+
+const newProfile = () => {
+    const reqName = fullname();
+    const reqHobbies = hobbyList();
+
+    return {
+        reqName,
+        reqHobbies
+    }
+}
+
+console.log(newProfile());
