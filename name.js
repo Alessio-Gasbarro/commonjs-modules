94a67f4a5d41e31c86783154d1e6997c6bc9f1c8
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-04-14 15:33:08 +0200

    [UPD] name.js export done

--- name.js
+++ name.js
@@ -1,5 +1,10 @@
 // Funzione che accetta firstName e lastName con Return
+let firstName = 'Alessio';
+let lastName = 'Gasbarro';
 
 const fullname = (firstName, lastName) => {
-    return firstName + lastName
+    return firstName + ' ' + lastName
 }
+
+//Export const 'fullname'
+module.exports = fullname;
