df46ec9a6610952363358034e1dd39aa9a0a10d2
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-04-14 15:50:55 +0200

    [UPD] made hobbies.js function + exports

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+//Funzione con 3 parametri "hobbyOne" "hobbyTwo" "hobbyThree", + return
+const hobbyList = (hobbyOne, hobbyTwo, hobbyThree) => {
+    return hobbyOne + ' ' + hobbyTwo + ' ' + hobbyThree;
+}
+
+//Exports funzione
+module.exports = hobbyList;
